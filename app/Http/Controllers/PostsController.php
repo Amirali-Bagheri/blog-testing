01e2f2310b3fa0303924c30867758bef4f696e1c
Amir <35amir80amir@gmail.com>
2022-06-07 13:10:54 +0430

    Fix PostController2

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,38 +7,74 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-	public function index()
-	{
-		//
-	}
+    public function index()
+    {
+        //
+    }
 
-	public function create()
-	{
-		//
-	}
+    public function create()
+    {
+        return view('dashboard.posts.create');
+    }
 
-	public function store(Request $request)
-	{
-		//
-	}
+    public function store(Request $request)
+    {
+        $request->validate([
+            "tilte" => 'required',
+            "content" => 'required',
+            "slug" => 'unique:posts,slug',
+        ]);
 
-	public function show(Post $post)
-	{
-		//
-	}
+        $user_id = auth()->id();
 
-	public function edit(Post $post)
-	{
-		//
-	}
+        Post::create([
+            "title" => $request->title,
+            "content" => $request->content,
+            "slug" => $request->slug,
+            "user_id" => $user_id
+        ]);
 
-	public function update(Request $request, Post $post)
-	{
-		//
-	}
+        return redirect()->route('dashboard')->with("success", "عملیات با موفقیت انجام شد");
+    }
 
-	public function destroy(Post $post)
-	{
-		//
-	}
+    public function show(Request $request)
+    {
+        $slug = $request->slug;
+        $post = Post::where("slug", $slug)->firstOrFail();
+        return view("blog.single", ["post" => $post]);
+    }
+
+    public function edit(Request $request)
+    {
+        $slug = $request->slug;
+        $post = Post::where("slug", $slug)->firstOrFail();
+        return view("dashboard.posts.create", ["post" => $post]);
+    }
+
+    public function update(Request $request, Post $post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        $request->validate([
+            "tilte" => 'required',
+            "content" => 'required',
+            "slug" => 'unique:posts,slug',
+        ]);
+
+        $user_id = auth()->id();
+
+        $post->update([
+            "title" => $request->title,
+            "content" => $request->content,
+            "slug" => $request->slug,
+        ]);
+
+        return redirect()->route('dashboard')->with("success", "عملیات با موفقیت انجام شد");
+    }
+
+    public function destroy(Post $post_id)
+    {
+        $post = Post::findOrFail($post_id);
+        $post->delete();
+    }
 }
